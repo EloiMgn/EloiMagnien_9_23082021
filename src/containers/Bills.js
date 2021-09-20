122eb0e6e1061b44df44e536642b9893ec005c92
EloiMgn <eloi.magnien@gmail.com>
2021-09-20 11:25:07 +0200

    validation format image et suppression bill non valides

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -23,7 +23,7 @@ export default class {
 
   handleClickIconEye = (icon) => {
     const billUrl = icon.getAttribute("data-bill-url")
-    console.log(billUrl);
+    console.log(icon);
     const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
     $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} /></div>`)
     $('#modaleFile').modal('show')
@@ -61,16 +61,9 @@ export default class {
           .filter(bill => bill.email === userEmail)
           // Delete bills with date that don't match with the requested format 
           .filter(bill => bill.date.match(/^\d{1,2}\s\w{3}\.\s\d{2}$/gmi) !== null)
+          // Delete bills with fileUrl not valid 
+          .filter(bill => bill.fileUrl !== null)
 
-          // .forEach(bill => {
-          // //  console.log(bill.date);
-          //  const words = bill.date.split(' ')
-          //  for (let i = 0; i < months.length; i++) {
-          //    if (words[1] === months[i].shortName) {
-          //       bill.date.replace(words[1], months[i].num)
-          //    }
-          //   }
-          // })
           console.log(bills);
           console.log('length', bills.length)
         return bills
--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -19,35 +19,41 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
-    this.firestore
-      .storage
-      .ref(`justificatifs/${fileName}`)
-      .put(file)
-      .then(snapshot => snapshot.ref.getDownloadURL())
-      .then(url => {
-        this.fileUrl = url
-        this.fileName = fileName
-      })
+    const fileType = file.type
+      this.firestore
+        .storage
+        .ref(`justificatifs/${fileName}`)
+        .put(file)
+        .then(snapshot => snapshot.ref.getDownloadURL())
+        .then(url => {
+          this.fileUrl = url
+          this.fileName = fileName
+          this.fileType = fileType
+        })
   }
   handleSubmit = e => {
     e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
-    const email = JSON.parse(localStorage.getItem("user")).email
-    const bill = {
-      email,
-      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: 'pending'
+    // ==↓↓↓↓↓↓== ajout validation format image ===↓↓↓↓↓↓==
+    if (this.fileType.includes("image")) {
+      console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
+      const email = JSON.parse(localStorage.getItem("user")).email
+      const bill = {
+        email,
+        type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+        name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+        amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+        date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+        vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+        pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+        commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+        fileUrl: this.fileUrl,
+        fileName: this.fileName,
+        fileType: this.fileType,
+        status: 'pending'
+      }
+      this.createBill(bill)
+      this.onNavigate(ROUTES_PATH['Bills'])
     }
-    this.createBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
   }
 
   // not need to cover this function by tests
